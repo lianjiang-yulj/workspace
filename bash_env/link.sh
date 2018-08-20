4366eb0f9310fede52ba82a153611865bba4b62c
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2018-08-20 20:19:10 +0800

    add go test

---
+++ bash_env/link.sh
@@ -0,0 +1,4 @@
+ln -s /home/lianjiang.yulj/workspace.github/bash_env/bash_profile /home/lianjiang.yulj/.bash_profile
+ln -s /home/lianjiang.yulj/workspace.github/bash_env/gitconfig /home/lianjiang.yulj/.gitconfig 
+ln -s /home/lianjiang.yulj/workspace.github/bash_env/gdbinit.gdb-8.1.0 /home/lianjiang.yulj/.gdbinit
+ln -s /home/lianjiang.yulj/workspace.github/bash_env/go_profile /home/lianjiang.yulj/.profile
