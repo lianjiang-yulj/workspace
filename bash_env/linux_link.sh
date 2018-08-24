273a9dd6b69237b24ce419d9dc178f034f2d1f9a
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2018-08-24 15:57:57 +0800

    add install url for anaconda

--- bash_env/linux_link.sh
+++ bash_env/linux_link.sh
@@ -2,7 +2,7 @@ link_path=`readlink -f $0`
 echo -e "bash $link_path \n"
 
 dir=`dirname $link_path`
-for f in bash_profile gitconfig gdbinit profile gitignore
+for f in bash_profile gitconfig gdbinit profile gitignore bashrc
 
 do
 cmd="ln -s $dir/$f $HOME/.$f"
