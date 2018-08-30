2f2bb53ef45e8c7d4994bb2fc358e497e3ef953a
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2018-08-30 20:15:50 +0800

    modify c++17 build options

--- cxx_standard_tools/build.sh
+++ cxx_standard_tools/build.sh
@@ -1,1 +1,7 @@
-g++ -g -ggdb -o a c++17.cc -std=c++17
+#!/bin/sh
+
+g++ -g -ggdb -o c++17.g++ c++17.cc -std=c++17
+
+#g++ -g -ggdb -o c++17.g++ c++17.cc -std=c++2a, you can set -std=c++2b to see all the c++1x standards
+
+
--- cxx_standard_tools/clang/build.sh
+++ cxx_standard_tools/clang/build.sh
@@ -1,1 +1,3 @@
-./clang/bin/clang++ ../c++14.cc -std=c++17 -g -ggdb -o a.bin
+#!/bin/sh
+
+./clang/bin/clang++ ../c++17.cc -std=c++17 -g -ggdb -o c++17.clang
