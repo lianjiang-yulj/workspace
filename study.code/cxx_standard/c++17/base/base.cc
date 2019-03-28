481e3663aa2b1eb590b4eee9d21170adfa408a5d
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2019-03-28 15:49:48 +0800

    add base

---
+++ study.code/cxx_standard/c++17/base/base.cc
@@ -0,0 +1,12 @@
+//
+// Created by lianjiang.yulj on 2019-01-02.
+//
+
+#include <iostream>
+
+
+void test() {
+
+	std::cout << "test" << std::endl;
+
+}
--- study.code/cxx_standard/c++17/main.cpp
+++ study.code/cxx_standard/c++17/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+
+#include "base/base.h"
+
 int main() {
 	std::cout << "Hello, World! c++17" << std::endl;
+
+	std::cout << "helloo" << std::endl;
+
+	test();
+
 	return 0;
 }
