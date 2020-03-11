d2b0aec779db59f87619ede7dc4c76a31ddfc864
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2020-03-11 21:35:31 +0800

    update bashenv

--- opensource/anaconda/update.sh
+++ opensource/anaconda/update.sh
@@ -1,11 +1,11 @@
 #/bin/sh
 
-conda init bash
-conda init zsh
-conda deactivate
+#conda init bash
+#conda init zsh
+#conda deactivate
 conda update conda
 conda update anaconda
-conda update anaconda-navigator    //update最新版本的anaconda-navigator
+conda update anaconda-navigator    #update最新版本的anaconda-navigator
 conda update --all
 
-conda update --prefix /Users/lianjiang.yulj/anaconda3 anaconda
+conda update --prefix ~/anaconda3 anaconda
