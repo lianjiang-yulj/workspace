adcd1e017ced64a01d148f13cdb0d80ed2ad56d4
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2020-03-11 17:56:31 +0800

    update conda

--- opensource/anaconda/update.sh
+++ opensource/anaconda/update.sh
@@ -1,6 +1,9 @@
 #/bin/sh
 
+conda deactivate
 conda update conda
 conda update anaconda
 conda update anaconda-navigator    //update最新版本的anaconda-navigator
 conda update --all
+
+conda update --prefix /Users/lianjiang.yulj/anaconda3 anaconda
