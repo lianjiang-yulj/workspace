46711a587138cb20b1261a2a284b6e8371095e20
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2020-03-12 10:15:23 +0800

    update

--- opensource/anaconda/update.sh
+++ opensource/anaconda/update.sh
@@ -3,6 +3,7 @@
 #conda init bash
 #conda init zsh
 #conda deactivate
+#conda update -n base conda
 conda update conda
 conda update anaconda
 conda update anaconda-navigator    #update最新版本的anaconda-navigator
