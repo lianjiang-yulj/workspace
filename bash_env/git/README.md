431ba7956b0bbe9122298f7153841881008922ca
lianjiang-yulj <lianjiang.yulj@users.noreply.github.com>
2020-05-17 23:23:41 +0800

    config github git config. add user/email

--- bash_env/git/README.md
+++ bash_env/git/README.md
@@ -1,2 +1,6 @@
 git config  core.filemode false
 #in your personal project
+
+如果要提交github，请在workspace.github中执行
+
+git_push_config_init_github.sh
---
+++ git_push_config_init_github.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+git config user.email lianjiang.yulj@users.noreply.github.com
+
+git config user.name lianjiang-yulj
+
+git config  core.filemode false
