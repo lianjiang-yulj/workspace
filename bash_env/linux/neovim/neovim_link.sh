217dbf1f3505e13532d9fadecf2e4e1dbd17cb83
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2020-09-24 10:13:10 +0800

    modify bashenv for nvim

---
+++ bash_env/linux/neovim/neovim_link.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo ln -s /home/lianjiang.yulj/workspace.github/bash_env/linux/neovim/nvim.appimage /usr/local/bin/nvim2
+sudo ln -s /home/lianjiang.yulj/workspace.github/bash_env/linux/neovim/squashfs-root/usr/bin/nvim /usr/local/bin/nvim
