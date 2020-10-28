896b3d72ba9f932941783ecd16c239ce49d3323c
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2020-10-28 10:33:24 +0800

    modifyupdate_local_brew_conda.sh

--- tools/update_local_brew_conda.sh
+++ tools/update_local_brew_conda.sh
@@ -1,24 +1,31 @@
 #!/bin/zsh
 
-#Usage: zsh update_local_brew_conda.sh
+#Usage: zsh update_local_brew_conda.sh 0/1/2
 
 #update python3.8.5
 #conda install python=3.8.5 -c anaconda
 #
-brew upgrade
-brew update
 
-omz update
+echo $1
 
-conda init zsh
+if [ "x$1" = "x0" ]; then
+    brew upgrade
+    brew update
+elif [ "x$1" = "x1" ]; then
+    . ~/.zshrc
+    omz update
+else
 
-conda deactivate
-conda update -c defaults conda
-conda update -n base -c defaults conda
-conda update conda
-conda update anaconda
-conda update anaconda-navigator
+    conda init zsh
 
-conda update --all
-conda update --prefix /opt/anaconda3 --update-all
-conda update --prefix /opt/anaconda3 anaconda
+    conda deactivate
+    conda update -c defaults conda
+    conda update -n base -c defaults conda
+    conda update conda
+    conda update anaconda
+    conda update anaconda-navigator
+
+    conda update --all
+    conda update --prefix /opt/anaconda3 --update-all
+    conda update --prefix /opt/anaconda3 anaconda
+fi
