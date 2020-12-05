87dfe20ec9f920e89a35f96645849092b7898e0f
lianjiang.yulj <lianjiang.yulj@alibaba-inc.com>
2020-12-05 16:37:11 +0800

    change default conda env

--- tools/update_local_brew_conda.sh
+++ tools/update_local_brew_conda.sh
@@ -20,31 +20,35 @@ else
 
     conda deactivate
 
-# 先更新 conda，然后更新anaconda
+echo "# 先更新 conda，然后更新anaconda"
     conda update conda -y
-    conda update anaconda -y
-    conda update anaconda-navigator -y
     conda update -c defaults conda -y
+    conda update anaconda -y
+    conda update anaconda -c defaults -y
+    conda update anaconda-navigator -y
+    conda update anaconda-navigator -c defaults -y
 
 
-# 更新/opt/anaconda3
-    conda update --prefix /opt/anaconda3 conda -y
-    conda update --prefix /opt/anaconda3 anaconda -y
-    conda update --prefix /opt/anaconda3 anaconda-navigator -y
-    conda update --prefix /opt/anaconda3 --update-all -y
+echo "# 更新/opt/anaconda3"
+    conda update --prefix /opt/anaconda3 conda -c defaults -y
+    conda update --prefix /opt/anaconda3 anaconda -c defaults -y
+    conda update --prefix /opt/anaconda3 anaconda-navigator -c defaults -y
+    conda update --prefix /opt/anaconda3 --update-all -c defaults -y
 
-# 然后更新每个环境的包，这里面每个环境并没有安装conda，所以更新无效，可以换成想更新的包
+echo "# 然后更新每个环境的包，这里面每个环境并没有安装conda，所以更新无效，可以换成想更新的包"
     conda update -n base -c defaults conda -y
     conda update -n sas -c defaults conda -y
     conda update -n pythonProject -c defaults conda -y
     conda update -n py38 -c defaults conda -y
     conda update -n zipline -c defaults conda -y
+    conda update -n quantitative -c defaults conda -y
 
-#更新每一个环境的所有包
+echo "#更新每一个环境的所有包"
     conda update --all -y
     conda update -n py38 -c defaults --update-all -y
     conda update -n pythonProject -c defaults --update-all -y
     conda update -n zipline -c defaults --update-all -y
     conda update -n base -c defaults --update-all -y
     conda update -n sas -c defaults --update-all -y
+    conda update -n quantitative -c defaults --update-all -y
 fi
