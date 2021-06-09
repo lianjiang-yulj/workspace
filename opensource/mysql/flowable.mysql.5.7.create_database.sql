a6aff699953e8dd97c46a3cb4549451edfe10f06
lianjiang.yulj <yuljasc@gmail.com>
2021-06-10 01:06:54 +0800

    update env

---
+++ opensource/mysql/flowable.mysql.5.7.create_database.sql
@@ -0,0 +1,22 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql5.7
+ Source Server Type    : MySQL
+ Source Server Version : 50727
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : jeecg-boot-os-re
+
+ Target Server Type    : MySQL
+ Target Server Version : 50727
+ File Encoding         : 65001
+
+ Date: 05/04/2021 18:44:42
+*/
+DROP DATABASE IF EXISTS `flowable-test`;
+create database `flowable-test` default character set utf8mb4 collate utf8mb4_general_ci;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+USE `flowable-test`;
+
